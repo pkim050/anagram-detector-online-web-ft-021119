93256839419154e85bb630a0b6e3c67dc4ec41d4
Patrick Kim <pkim050@gmail.com>
2019-02-20 21:14:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  @@arr = []
+  
+  def initialize(string)
+    @word = string
+  end
+  
+  def match(array)
+    @@arr.clear
+    array.each do |element|
+      @@arr << element if element.chars.sort.join == @word.chars.sort.join
+    end
+    @@arr
+  end
+end
